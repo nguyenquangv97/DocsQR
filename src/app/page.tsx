3bb3b92be9ba046cb674ff550ad86e6f4d8c7cbe
Quang Nguyen <nguyenquangv97@gmail.com>
2024-06-19 21:17:10 -0700

    [ADD] landing page HERO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <MaxWidthWrapper>
+      <div className="w-full h-full flex justify-center items-center">
+        <h1 className="font-bold text-5xl">Landing Page</h1>
+      </div>
+    </MaxWidthWrapper>
+  );
+};
 
-export default Home
+export default Home;
