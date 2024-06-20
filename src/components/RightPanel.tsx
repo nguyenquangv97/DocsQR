9cac25acd694fef633d193c7569e698e556a0523
Quang Nguyen <nguyenquangv97@gmail.com>
2024-06-19 21:33:20 -0700

    chore: Update RightPanel image dimensions to improve display

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 const RightPanel = () => {
-  return <Image src="/images/hero.jpg" alt="hero" width={500} height={500} className="rounded-2xl"/>;
+  return <Image src="/images/hero.jpg" alt="hero" width={800} height={800} className="rounded-2xl"/>;
 };
 
 export default RightPanel;
